930e8090030009f5b92754fb73b3a6b70e4b22e7
Robin Persson <rp.ullstorp92@gmail.com>
2022-06-20 09:18:10 +0200

    Created test class

---
+++ backend/src/main/java/com/example/demo/TodoTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TodoTest {
+    /*
+    Result result = JUnitCore.runClasses(TestEmployeeDetails.class);
+
+      for (Failure failure : result.getFailures()) {
+        System.out.println(failure.toString());
+    }
+     */
+
+}
